302d07163deda44a018bf40008f0d5ce9b3457dc
ChunhuiDiao <15621495807@sina.cn>
2018-05-06 22:25:28 +0800

    初步认识后台模块化开发

---
+++ node学习历程/day1/模快化/index.js
@@ -0,0 +1,4 @@
+//通过require将上面建立的test1模块引入
+var test = require('./test1.js')
+//执行test1下面的con方法
+test.con()
---
+++ node学习历程/day1/模快化/test1.js
@@ -0,0 +1,4 @@
+//export 是将该接口暴漏，方便外部函数访问
+exports.con = function(){
+	console.log("我是test1下面的输出")
+}
